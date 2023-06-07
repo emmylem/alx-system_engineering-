c651aab9e98367c3d41e307838a8524b644daf07
emmylem <benjaminosondu55@gmail.com>
2023-06-07 13:17:12 +0000

    word-pr.php debugged

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fixing phpp extentions to php in wordpress file.
 
 exec { 'wordpress-fix':
-  command => 'sed -i s/phpp/php/g /var/html/word-pr.php',
+  command => 'sed -i s/phpp/php/g /var/html/wp-settings.php',
   path  => '/usr/local/bin/:bin/'}
